02a681c72905eebb5ede3a74f318869b2c37d740
Brittany Ohalete <brittanyohalete96@gmail.com>
2017-10-06 00:20:34 -0400

    Font and Images folder
    css styling and added form

---
+++ mylib.js
@@ -0,0 +1,6 @@
+function goFunc(){
+  window.alert('Hello '+ document.feedbackform.fname.value + ' ' + document.feedbackform.lname.value
+  + '\n Email: ' + document.feedbackform.email.value
+  + '\n Phone: ' + document.feedbackform.telnum.value
+  + '\n Comments: ' +document.feedbackform.comments.value);
+}
